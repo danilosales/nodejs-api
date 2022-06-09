2dcdb0e5fe38c427d88fc77c7bfec07f7327e476
João Gomes <devjoaoantoniogomes@gmail.com>
2022-06-09 13:34:29 -0300

    Config dockerfile

--- database.js
+++ database.js
@@ -1,5 +1,15 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('postgres://postgres:postgres@localhost:5432/cadastro_pessoas', {dialect: 'postgres'});
+const sequelize = new Sequelize(process.env.DB_DATABASE || 'cadastro_pessoas',
+    process.env.DB_USER || 'postgres',
+    process.env.DB_PASSWORD || '',
+    {
+      host: process.env.DB_HOST || 'localhost',
+      port: process.env.DB_PORT || 5432,
+      dialect: 'postgres',
+      dialectOptions: {
+        ssl: process.env.DB_SSL == "true"
+      }
+    });
 
 
 const User = sequelize.define('user', {
