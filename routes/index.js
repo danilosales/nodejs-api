5079b432b5872c69bf6746d07f70bb97e6e3e06c
João Gomes <devjoaoantoniogomes@gmail.com>
2022-06-10 13:22:05 -0300

    fix(env): fix env variables to node interpreter

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('Hello World!');
+  res.send(`Hello World!\nDB port: ${process.env.DB_PORT}\nDB host: ${process.env.DB_HOST}\nDB database: ${process.env.DB_DATABASE}`);
 });
 
 module.exports = router;
